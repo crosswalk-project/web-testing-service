cb029afee9bc17e8106dd6ec7dd1373cad4e2f9b
li jun <junx.b.li@intel.com>
2015-01-19 11:02:39 +0800

    Update tests on WW04.1

--- wts/tests/sse/w3c/resources/message.py
+++
@@ -1,9 +0,0 @@
-def main(request, response):
-    mime = request.GET.first("mime", "text/event-stream")
-    message = request.GET.first("message", "data: data");
-    newline = "" if request.GET.first("newline", None) == "none" else "\n\n";
-
-    headers = [("Content-Type", mime)]
-    body = message + newline + "\n"
-
-    return headers, body
--- wts/tests/xmlhttprequest/w3c/resources/form.py
+++
@@ -1,5 +0,0 @@
-def main(request, response):
-    try:
-        return "id:%s;value:%s;" % (request.POST.first("id"), request.POST.first("value"))
-    except Exception,ex:
-        return "error"
