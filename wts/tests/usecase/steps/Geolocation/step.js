86f664b965227d11f4c645a55c742d9556294b92
li jun <junx.b.li@intel.com>
2015-06-29 10:14:54 +0800

    Update tests on WW26.5

---
+++ wts/tests/usecase/steps/Geolocation/step.js
@@ -0,0 +1,8 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verifies the location information of latitude and longitude position can be fetched.</p>'
+            +'<p>Test Pre-configuration: </p>'
+            +'<p>Enable location service(via GPS, cellular network or wifi)</p>'
+            +'<p>Expected Result: </p>'
+            +'<p>Test passes if both the latitude and longitude return the correct position data.</p>'
+          +'</font>';
